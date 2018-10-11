b35fd9fe8745a4bcb9c3f9de1943709d5b6282d2
srijanreddy98 <srijanreddy98@gmail.com>
2018-10-11 16:40:00 +0530

    Top bar done

---
+++ almamatters/src/app/dashboard/components/leftbar/leftbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leftbar',
+  templateUrl: './leftbar.component.html',
+  styleUrls: ['./leftbar.component.scss']
+})
+export class LeftbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- almamatters/src/app/dashboard/dashboard.module.ts
+++ almamatters/src/app/dashboard/dashboard.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LeftbarComponent } from './components/leftbar/leftbar.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FlexLayoutModule
   ],
-  declarations: [DashboardComponent, TopBarComponent]
+  declarations: [DashboardComponent, TopBarComponent, LeftbarComponent]
 })
 export class DashboardModule { }
