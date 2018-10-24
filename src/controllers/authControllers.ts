2961688623c86888e36073c9b7cd79fee12fd0d1
srijanreddy98 <srijanreddy98@gmail.com>
2018-10-24 18:50:18 +0530

    Auth started

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,3 +1,4 @@
+import {sign as signToken, verify as verifyToken } from 'jsonwebtoken';
 const authIndex = async (req, res) => {
     res.json({ message: 'who the f are you?' })
 };
@@ -6,4 +7,22 @@ const jesseIndex = (req, res) => {
     res.json({ message: 'jesse is the best' })
 };
 
-export { authIndex, jesseIndex };
+const getDetails = async (req, res) => {
+    const token = req.body.token;
+    // const user = await User.findById(req.body.id);
+    // if (user == null) res.send({error: 'UserId not present'});
+    let decoded;
+    try {
+        decoded = verifyToken(req.body.token, req.body.secret);
+
+        // TODO find row by id in token and return the scope
+        // const student = await queryDb(`select * from student where Id=${decoded.id}`)
+
+        // TODO create token and send in response
+        res.send({});
+    } catch(e) {
+        res.send(e)
+    }
+}
+
+export { authIndex, jesseIndex, getDetails };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { authIndex, jesseIndex } from '../controllers/authControllers';
+import { authIndex, jesseIndex, getDetails } from '../controllers/authControllers';
 
 const router = new Router();
 
 router.get('/jesse', jesseIndex);
+router.get('/getToken', getDetails);
 router.get('/*', authIndex);
 
 export default router;
