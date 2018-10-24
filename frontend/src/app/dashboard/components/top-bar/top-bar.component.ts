26e0f4cb372e793b3eda90b4d0cffb4a23c906cd
srijanreddy98 <srijanreddy98@gmail.com>
2018-10-24 11:18:41 +0530

    Merging

--- frontend/src/app/dashboard/components/top-bar/top-bar.component.ts
+++ frontend/src/app/dashboard/components/top-bar/top-bar.component.ts
@@ -1,21 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { User } from './../../../models/user.model';
-import { AppState } from './../../../app.state';
-import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
-  user: User;
-  constructor(private store: Store<AppState>) { 
-    store.select('user').subscribe(
-      user => this.user = user
-    );
-   }
-
+  constructor(private store: Store<AppState>) { }
   ngOnInit() {
   }
 
