16640ff554d181afe882fcb269555f9c1f962b7e
rutvikvijjali <30809348+rutvikvijjali@users.noreply.github.com>
2018-10-25 22:29:27 +0530

    Revert "Frontend/profile"

--- frontend/src/app/dashboard/components/main-dashboard/main-dashboard.component.ts
+++ frontend/src/app/dashboard/components/main-dashboard/main-dashboard.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { User } from './../../../models/user.model';
-import { AppState } from './../../../app.state';
-import * as UserActions from '../../../actions/user.action';
+
 @Component({
   selector: 'app-main-dashboard',
   templateUrl: './main-dashboard.component.html',
@@ -10,11 +7,9 @@ import * as UserActions from '../../../actions/user.action';
 })
 export class MainDashboardComponent implements OnInit {
 
-  constructor(private store: Store<AppState>) { }
+  constructor() { }
 
   ngOnInit() {
   }
-  changeUser() {
-    this.store.dispatch(new UserActions.UpdateUser({name: 'Anurag Gupta', token: 'SomeShit'}) );
-  }
+
 }
