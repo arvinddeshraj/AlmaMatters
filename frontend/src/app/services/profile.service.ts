6cca575d41346fb555b83c81a2786764b1b4dc2c
srijanreddy98 <srijanreddy98@gmail.com>
2018-10-25 23:12:01 +0530

    Added profile service

---
+++ frontend/src/app/services/profile.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private http: HttpClient) { }
+
+  getProfile(token) {
+    return this.http.get('/api/profile/',{headers: {'x-auth': token}});
+  }
+}
